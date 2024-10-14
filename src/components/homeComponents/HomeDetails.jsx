4c1c513b707a1cc137d5763a5f9954af7e7ae734
sai-ram-kaushik <sairamkaushik1@gmail.com>
2024-10-14 11:23:35 +0530

    updated the home page

--- src/components/homeComponents/HomeDetails.jsx
+++ src/components/homeComponents/HomeDetails.jsx
@@ -1,97 +1,128 @@
 import React, { useEffect, useState } from "react";
 import data from "../../data/Home.json";
 import axios from "axios";
+import { Link } from "react-router-dom";
 const HomeDetails = () => {
   const [count, setCount] = useState("");
-  const [userCount, setUserCount] = useState("");
+  const [orderPendingCount, setOrderPendingCount] = useState("");
+  const [storeAdminCount, setStoreAdminCount] = useState("");
   const { totalProduct, orderPending, totalCustomer } = data.productDetails;
 
   useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
     axios
-      .get(`http://localhost:8000/api/admin/get-catalogs-count`)
+      .get(
+        `${import.meta.env.VITE_APP_API_ENDPOINT}/api/store/count-products`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
       .then((response) => {
         setCount(response.data.data);
-        console.log(response.data.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching the product count", error);
       });
   }, []);
 
   useEffect(() => {
     axios
-      .get(`${import.meta.env.VITE_API_ENDPOINT_URI}/api/users/get-users-count`)
+      .get(
+        `${import.meta.env.VITE_APP_API_ENDPOINT}/api/store/order-pending-count`
+      )
       .then((response) => {
-        setUserCount(response.data.data);
-      })
-      .catch((error) => {
-        console.log(
-          "getting error while fetching the count of the users",
-          error
-        );
+        setOrderPendingCount(response.data.data);
+      });
+  });
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
+    axios
+      .get(
+        `${import.meta.env.VITE_APP_API_ENDPOINT}/api/public/total-customers`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        setStoreAdminCount(response.data.data);
       });
   }, []);
 
   return (
     <div className="w-full">
       <div className="flex flex-col md:flex-row items-center gap-4 w-full">
-        <div className="bg-[#DEE9FF] md:min-w-[374.67px] md:min-h-[175px] rounded-lg w-full">
-          <div className="flex flex-col items-start gap-3 py-[28px] px-[32px]">
-            <div className="flex items-center gap-4">
-              <img src={totalProduct.icon} width={32} height={32} />
-              <h3 className="text-[24px] font-heading font-semibold">
-                {totalProduct.title}
-              </h3>
-              <img src={totalProduct.arrowIcon} />
-            </div>
-            <div className="flex items-center gap-2">
-              <h3 className="text-[56px] font-bold">{count}</h3>
-              <div className="flex flex-col items-start">
-                <p>10 New products</p>
-                <p>added last month</p>
+        <Link to="/store-admin/dashboard/products" className="w-full">
+          <div className="bg-[#DEE9FF] md:min-w-[374.67px] md:min-h-[175px] rounded-lg w-full">
+            <div className="flex flex-col items-start gap-3 py-[28px] px-[32px]">
+              <div className="flex items-center gap-4">
+                <img src={totalProduct.icon} width={32} height={32} />
+                <h3 className="text-[24px] font-heading font-semibold">
+                  {totalProduct.title}
+                </h3>
+                <img src={totalProduct.arrowIcon} />
               </div>
-            </div>
-          </div>
-        </div>
 
-        <div className="bg-[#FBE0FF] md:min-w-[374.67px] md:min-h-[175px] rounded-lg w-full">
-          <div className="flex flex-col items-start gap-3 py-[28px] px-[32px]">
-            <div className="flex items-center gap-4">
-              <img src={orderPending.icon} width={32} height={32} />
-              <h3 className="text-[24px] font-heading font-semibold">
-                {orderPending.title}
-              </h3>
-              <img src={orderPending.arrowIcon} />
-            </div>
-            <div className="flex items-center gap-2">
-              <h3 className="text-[56px] font-bold">
-                {orderPending.totalNumber}
-              </h3>
-              <div className="flex flex-col items-start">
-                <p>10 New products</p>
-                <p>added last month</p>
+              <div className="flex items-center gap-2">
+                <h3 className="text-[56px] font-bold">{count.count}</h3>
+                <div className="flex flex-col items-start">
+                  <p>10 New products</p>
+                  <p>added last month</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="bg-[#FFEDD3] md:min-w-[374.67px] md:min-h-[175px] rounded-lg w-full">
-          <div className="flex flex-col items-start gap-3 py-[28px] px-[32px]">
-            <div className="flex items-center gap-4">
-              <img src={totalCustomer.icon} width={32} height={32} />
-              <h3 className="text-[24px] font-heading font-semibold">
-                {totalCustomer.title}
-              </h3>
-              <img src={totalCustomer.arrowIcon} />
-            </div>
-            <div className="flex items-center gap-2">
-              <h3 className="text-[56px] font-bold">
-                {totalCustomer.totalNumber}
-              </h3>
-              <div className="flex flex-col items-start">
-                <p>10 New products</p>
-                <p>added last month</p>
+        <Link to="/store-admin/dashboard/orders" className="w-full">
+          <div className="bg-[#FBE0FF] md:min-w-[374.67px] md:min-h-[175px] rounded-lg w-full">
+            <div className="flex flex-col items-start gap-3 py-[28px] px-[32px]">
+              <div className="flex items-center gap-4">
+                <img src={orderPending.icon} width={32} height={32} />
+                <h3 className="text-[24px] font-heading font-semibold">
+                  {orderPending.title}
+                </h3>
+                <img src={orderPending.arrowIcon} />
+              </div>
+              <div className="flex items-center gap-2">
+                <h3 className="text-[56px] font-bold">{orderPendingCount}</h3>
+                <div className="flex flex-col items-start">
+                  <p>10 New products</p>
+                  <p>added last month</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
+
+        <Link to="/store-admin/dashboard/total-customers" className="w-full">
+          <div className="bg-[#FFEDD3] md:min-w-[374.67px] md:min-h-[175px] rounded-lg w-full">
+            <div className="flex flex-col items-start gap-3 py-[28px] px-[32px]">
+              <div className="flex items-center gap-4">
+                <img src={totalCustomer.icon} width={32} height={32} />
+                <h3 className="text-[24px] font-heading font-semibold">
+                  {totalCustomer.title}
+                </h3>
+                <img src={totalCustomer.arrowIcon} />
+              </div>
+              <div className="flex items-center gap-2">
+                <h3 className="text-[56px] font-bold">
+                  {storeAdminCount.userCount}
+                </h3>
+                <div className="flex flex-col items-start">
+                  <p>10 New products</p>
+                  <p>added last month</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
--- src/components/homeComponents/HomeStats.jsx
+++ src/components/homeComponents/HomeStats.jsx
@@ -1,22 +1,66 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Chart from "chart.js/auto";
-import data from "../../data/Home.json";
+import axios from "axios";
+
 const HomeStats = () => {
-  const { products } = data.sellingProducts.topSellingProduct;
   const chartContainer = useRef(null);
   const chartInstance = useRef(null);
+  const [topProducts, setTopProducts] = useState([]);
+  const [revenueData, setRevenueData] = useState([0, 0, 0, 0, 0, 0, 0, 0]); // Default revenue data for 8 months
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
+    // Fetch top selling products
+    axios
+      .get(`${import.meta.env.VITE_APP_API_ENDPOINT}/api/store/get-products`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setTopProducts(response.data.data);
+      });
+
+    // Fetch revenue data for months
+    axios
+      .get(
+        `${
+          import.meta.env.VITE_APP_API_ENDPOINT
+        }/api/store/get-revenue-per-month`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        const fetchedRevenueData = response.data.data.revenueByMonth;
+        setRevenueData(fetchedRevenueData); // Update revenue data with the fetched values
+      })
+      .catch((error) => {
+        console.error("Error fetching revenue data:", error);
+      });
+  }, []);
 
   useEffect(() => {
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"];
-    const revenueData = [
-      10000, 15000, 20000, 25000, 28000, 29000, 30000, 31000,
+    const backgroundColors = [
+      "#9C8AEF", // Jan
+      "#99FFB7", // Feb
+      "#0F0F0F", // Mar
+      "#99DEFF", // Apr
+      "#9EC4D8", // May
+      "#99FFB7", // Jun
+      "#99FFB7", // Jul
+      "#99FFB7", // Aug
     ];
 
     if (chartContainer.current) {
       const ctx = chartContainer.current.getContext("2d");
 
       if (chartInstance.current) {
-        chartInstance.current.destroy();
+        chartInstance.current.destroy(); // Destroy the previous chart instance to avoid re-render issues
       }
 
       chartInstance.current = new Chart(ctx, {
@@ -26,19 +70,49 @@ const HomeStats = () => {
           datasets: [
             {
               label: "Revenue",
-              backgroundColor: "rgba(54, 162, 235, 0.5)",
+              backgroundColor: backgroundColors,
               borderColor: "rgba(54, 162, 235, 1)",
               borderWidth: 2,
-              data: revenueData,
+              data: revenueData, // Set the updated revenue data
+              borderRadius: 10,
             },
           ],
         },
         options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              display: false,
+            },
+            title: {
+              display: true,
+              font: {
+                size: 20,
+              },
+              color: "#000",
+              align: "start",
+            },
+          },
           scales: {
             y: {
               beginAtZero: true,
-              stepSize: 10000,
-              max: 30000,
+              ticks: {
+                callback: function (value) {
+                  return value + "K";
+                },
+                color: "#B3B3B3",
+              },
+              grid: {
+                display: false,
+              },
+            },
+            x: {
+              ticks: {
+                color: "#B3B3B3",
+              },
+              grid: {
+                display: false,
+              },
             },
           },
         },
@@ -50,57 +124,55 @@ const HomeStats = () => {
         chartInstance.current.destroy();
       }
     };
-  }, []);
+  }, [revenueData]); // Re-render chart whenever revenueData changes
 
   return (
     <div className="w-full">
-      <div className="flex flex-col md:flex-row items-start gap-2 w-full">
-        <div className="md:min-w-[586px] md:min-h-[328px] bg-background rounded-lg p-[24px] w-full overflow-x-scroll">
+      <div className="flex flex-col lg:flex-row items-start gap-4 w-full">
+        <div className="lg:min-w-[586px] lg:min-h-[328px] bg-background rounded-lg p-6 w-full overflow-x-scroll">
           <div className="flex items-start">
-            <h3 className="text-[24px] font-semibold">Revenue so far</h3>
+            <h3 className="text-xl font-semibold">Revenue so far</h3>
           </div>
           <canvas ref={chartContainer}></canvas>
         </div>
 
-        <div className="min-w-[562px] min-h-[328px] bg-background rounded-lg p-[24px] w-full">
+        <div className="lg:min-w-[562px] lg:min-h-[328px] bg-background rounded-lg p-6 w-full">
           <div className="flex flex-col items-start w-full">
             <div className="flex items-center justify-between w-full">
-              <h3 className="text-[24px] font-semibold">Top selling product</h3>
-              <p className="text-secondary">See All &rarr; </p>
+              <h3 className="text-xl font-semibold">Top selling product</h3>
+              <p className="text-secondary cursor-pointer">See All &rarr;</p>
             </div>
 
             <div className="flex flex-col items-start gap-4 mt-4 w-full">
-              {products.map((product, index) => {
-                return (
-                  <div
-                    key={index}
-                    className="flex items-center justify-between border w-full p-3 rounded-lg"
-                  >
-                    <div className="flex items-center gap-2">
-                      <img src={product.image} />
-                      <p className="max-w-[200px] text-[16px] leading-[21.82px] text-secondary">
-                        {product.label}
-                      </p>
+              {topProducts.slice(0, 4).map((product, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col md:flex-row items-center justify-between border w-full p-3 rounded-lg"
+                >
+                  <div className="flex items-center gap-2">
+                    <img
+                      src={product.image}
+                      alt={product.label}
+                      className="w-12 h-12 object-cover rounded-md"
+                    />
+                    <p className="max-w-[200px] text-base leading-6 text-secondary">
+                      {product.label}
+                    </p>
+                  </div>
+
+                  <div className="flex items-center gap-4 mt-2 md:mt-0">
+                    <div className="flex flex-col items-start">
+                      <p className="text-xs text-paraHelper">UNITS SOLD</p>
+                      <p>{product.unitsSold} units</p>
                     </div>
 
-                    <div className="flex items-center gap-4">
-                      <div className="flex flex-col items-start">
-                        <p className="text-[10px] text-paraHelper">
-                          UNITS SOLD
-                        </p>
-                        <p>{product.unitsSold} units</p>
-                      </div>
-
-                      <div className="flex flex-col items-start">
-                        <p className="text-[10px] text-paraHelper">
-                          TOTAL SALES
-                        </p>
-                        <p>{product.totalSales}</p>
-                      </div>
+                    <div className="flex flex-col items-start">
+                      <p className="text-xs text-paraHelper">TOTAL SALES</p>
+                      <p>{product.totalSales}</p>
                     </div>
                   </div>
-                );
-              })}
+                </div>
+              ))}
             </div>
           </div>
         </div>
